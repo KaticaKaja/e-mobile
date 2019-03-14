8aeebf3317ebb0fd7423427a24fe15befa105806
Katica Rankovic <katicar@outlook.com>
2019-03-14 01:21:06 +0100

    ajdeeE

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -27,13 +27,19 @@ function renderNav(links){
                     <a class="nav-link" href="${link.href}">${link.content}</a>
                 </li>`;
         }
-        if(currentLocation==link.href){
+        else if(currentLocation == "" && link.href!="index.html"){
+            html+=`
+                <li class="nav-item">
+                    <a class="nav-link" href="${link.href}">${link.content}</a>
+                </li>`;
+        }
+        if(currentLocation==link.href && currentLocation!=""){
             html+=`
                 <li class="nav-item active">
                     <a class="nav-link" href="${link.href}">${link.content}</a>
                 </li>`;
         }
-        else if(currentLocation == "" && link.href!="index.html"){
+        else if(currentLocation!=""){
             html+=`
                 <li class="nav-item">
                     <a class="nav-link" href="${link.href}">${link.content}</a>

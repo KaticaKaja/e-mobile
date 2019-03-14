5103137648c3f0aeb1c33fa2327aee8104ba09a9
Katica Rankovic <katicar@outlook.com>
2019-03-14 01:14:17 +0100

    valjda ovako

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -21,13 +21,13 @@ function renderNav(links){
     let html = "";
     let currentLocation = window.location.pathname.split('/')[window.location.pathname.split('/').length-1];
     links.forEach(link=>{
-        if(currentLocation==link.href && currentLocation!=""){
+        if(currentLocation == "" && link.href=="index.html"){
             html+=`
                 <li class="nav-item active">
                     <a class="nav-link" href="${link.href}">${link.content}</a>
                 </li>`;
         }
-        if(currentLocation == ""){
+        if(currentLocation==link.href){
             html+=`
                 <li class="nav-item active">
                     <a class="nav-link" href="${link.href}">${link.content}</a>
